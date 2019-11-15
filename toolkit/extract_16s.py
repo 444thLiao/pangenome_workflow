6b7c5035f3617f60ad16cfe0f256b2fbf316a53b
thliao <l0404th@gmail.com>
2019-11-15 09:57:50 +0800

    fix bugs

---
+++ toolkit/extract_16s.py
@@ -0,0 +1,42 @@
+from Bio import SeqIO
+from glob import glob
+from os.path import *
+from tqdm import tqdm
+from collections import defaultdict
+indir = './pipelines_o/prokka_o'
+mode = 'prokka'
+
+
+name2_rrna = defaultdict(list)
+for f in tqdm(glob(join(indir,'*','*.gbk'))):
+    records = SeqIO.parse(f,format='genbank')
+    name = basename(dirname(f))
+    for r in records:
+        rrna = [_ for _ in r.features if _.type == 'rRNA']
+        if rrna:
+            rrna_16S = [_
+                        for _ in rrna
+                        if "16S ribosomal RNA" in _.qualifiers['product']]
+            if len(rrna_16S) == 1 and name2_rrna.get(name) is not None:
+                print('multiple 16s in ', name)
+                _locus_tag = rrna_16S[0].qualifiers['locus_tag']
+                _record = rrna_16S[0].extract(r)
+                _record.description = _record.id = _record.name
+                _record.id = _locus_tag[0]
+                name2_rrna[name].append(_record)
+            elif len(rrna_16S) == 1:
+                _locus_tag = rrna_16S[0].qualifiers['locus_tag']
+                _record = rrna_16S[0].extract(r)
+                _record.description = _record.id = _record.name
+                _record.id = _locus_tag[0]
+                name2_rrna[name].append(_record)
+            elif len(rrna_16S) > 1:
+                print('multiple 16s in ', name)
+
+    if name2_rrna.get(name) is None:
+        print('no 16s in ',name)
+
+ofile = '/home-user/thliao/script/EzbioCloudMatcher/seq'
+records = [_ for k,v in name2_rrna.items() for _ in v]
+with open(ofile,'w') as f1:
+    SeqIO.write(records,f1,format='fasta-2line')
