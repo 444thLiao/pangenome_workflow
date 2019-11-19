dafb921c35c9c1a7103e3ba006ac68f8650573f1
thliao <l0404th@gmail.com>
2019-11-19 10:20:30 +0800

    add usage

--- toolkit/extra_toolkit_luolab/extract_16s.py
+++ toolkit/extra_toolkit_luolab/extract_16s.py
@@ -3,46 +3,70 @@ from glob import glob
 from os.path import *
 from tqdm import tqdm
 from collections import defaultdict
-indir = './pipelines_o/prokka_o'
-mode = 'prokka'
 
 
-name2_rrna = defaultdict(list)
-for f in tqdm(glob(join(indir,'*','*.gbk'))):
-    records = SeqIO.parse(f,format='genbank')
-    name = basename(dirname(f))
-    for r in records:
-        rrna = [_ for _ in r.features if _.type == 'rRNA']
-        if rrna:
-            rrna_16S = [_
-                        for _ in rrna
-                        if "16S ribosomal RNA" in _.qualifiers['product']]
-            if len(rrna_16S) == 1 and name2_rrna.get(name) is not None:
-                print('multiple 16s in ', name)
-                _locus_tag = rrna_16S[0].qualifiers['locus_tag']
-                _record = rrna_16S[0].extract(r)
-                _record.description = _record.id = _record.name
-                _record.id = _locus_tag[0]
-                name2_rrna[name].append(_record)
-            elif len(rrna_16S) == 1:
-                _locus_tag = rrna_16S[0].qualifiers['locus_tag']
-                _record = rrna_16S[0].extract(r)
-                _record.description = _record.id = _record.name
-                _record.id = _locus_tag[0]
-                name2_rrna[name].append(_record)
-            elif len(rrna_16S) > 1:
-                for _16s in rrna_16S:
-                    _locus_tag = _16s.qualifiers['locus_tag']
-                    _record = _16s.extract(r)
+def extract_16s(indir):
+    name2_rrna = defaultdict(list)
+    for f in tqdm(glob(join(indir, '*', '*.gbk'))):
+        name = basename(dirname(f))
+        if not exists(f):
+            print('no sequence in ', name)
+            continue
+        records = SeqIO.parse(f, format='genbank')
+
+        for r in records:
+            # each r is a contig
+            rrna = [_ for _ in r.features if _.type == 'rRNA']
+            if rrna:
+                rrna_16S = [_
+                            for _ in rrna
+                            if "16S ribosomal RNA" in _.qualifiers['product']]
+                if len(rrna_16S) == 1 and name2_rrna.get(name) is not None:
+                    print('multiple 16s in ', name)
+                    _locus_tag = rrna_16S[0].qualifiers['locus_tag']
+                    _record = rrna_16S[0].extract(r)
                     _record.description = _record.id = _record.name
                     _record.id = _locus_tag[0]
                     name2_rrna[name].append(_record)
-                print('multiple 16s in ', name)
-    if name2_rrna.get(name) is None:
-        print('no 16s in ',name)
+                elif len(rrna_16S) == 1:
+                    _locus_tag = rrna_16S[0].qualifiers['locus_tag']
+                    _record = rrna_16S[0].extract(r)
+                    _record.description = _record.id = _record.name
+                    _record.id = _locus_tag[0]
+                    name2_rrna[name].append(_record)
+                elif len(rrna_16S) > 1:
+                    # multiple 16S within one contig
+                    for _16s in rrna_16S:
+                        _locus_tag = _16s.qualifiers['locus_tag']
+                        _record = _16s.extract(r)
+                        _record.description = _record.id = _record.name
+                        _record.id = _locus_tag[0]
+                        name2_rrna[name].append(_record)
+                    print('multiple 16s(single contig) in ', name)
+        if name2_rrna.get(name) is None:
+            print('no 16s in ', name)
+    return name2_rrna
 
 
-ofile = '/home-user/thliao/data/jjtao_20191113/16S.fasta'
-records = [_ for k,v in name2_rrna.items() for _ in v]
-with open(ofile,'w') as f1:
-    SeqIO.write(records,f1,format='fasta-2line')
+if __name__ == '__main__':
+    import sys
+
+    assert len(sys.argv) == 3
+
+
+    # extract_16s.py ./pipelines_o/prokka_o /home-user/thliao/data/jjtao_20191113/16S.fasta
+    def process_path(path):
+        if not '/' in path:
+            path = './' + path
+        return abspath(path)
+
+
+    #     indir = './pipelines_o/prokka_o'
+    # mode = 'prokka'
+    # ofile = '/home-user/thliao/data/jjtao_20191113/16S.fasta'
+    indir = process_path(sys.argv[1])
+    ofile = process_path(sys.argv[2])
+    name2_rrna = extract_16s(indir)
+    records = [_ for k, v in name2_rrna.items() for _ in v]
+    with open(ofile, 'w') as f1:
+        SeqIO.write(records, f1, format='fasta-2line')
--- toolkit/extra_toolkit_luolab/modify_prokka_o.py
+++ toolkit/extra_toolkit_luolab/modify_prokka_o.py
@@ -12,48 +12,62 @@ from collections import defaultdict
 import pandas as pd
 import os
 
-indir = './pipelines_o/prokka_o'
-mode = 'prokka'
-annotate_16s = './matchResults.csv'
+def main(indir,odir,annotate_16s,):
+    a = pd.read_csv(annotate_16s, sep='\t')
+    derep_a = a.drop_duplicates('sequence ID')
 
-a = pd.read_csv(annotate_16s, sep='\t')
-derep_a = a.drop_duplicates('sequence ID')
+    g2name = defaultdict(list)
+    for _, row in derep_a.iterrows():
+        id = row['sequence ID']
+        gname = id.split('_')[0]
+        taxon_name = row['taxon_name']
+        strain_name = row['strain_name']
+        if gname in g2name:
+            print('error', gname)
+            g2name.pop(gname)
+        else:
+            g2name[gname] = (taxon_name.replace(' ', '_'),
+                             str(strain_name).replace(' ', '_'))
 
-g2name = defaultdict(list)
-for _, row in derep_a.iterrows():
-    id = row['sequence ID']
-    gname = id.split('_')[0]
-    taxon_name = row['taxon_name']
-    strain_name = row['strain_name']
-    if gname in g2name:
-        print('error', gname)
-        g2name.pop(gname)
-    else:
-        g2name[gname] = (taxon_name.replace(' ', '_'),
-                         str(strain_name).replace(' ', '_'))
 
-odir = './new_proteins'
-if not exists(odir):
-    os.makedirs(odir)
-for g in tqdm(glob(join(indir, '*', '*.gbk'))):
-    records = SeqIO.parse(g, format='genbank')
-    gname = basename(dirname(g))
-    new_proteins = []
-    annoate_name = '_'.join(g2name.get(gname, []))
-    if not annoate_name:
-        continue
-    for r in records:
-        CDS_records = [_ for _ in r.features if _.type == 'CDS']
-        for cds in CDS_records:
-            _r = cds.translate(r)
-            _r.id = _r.name = _r.description = ''
-            q = cds.qualifiers
-            locus_tag = q['locus_tag'][0]
-            product = ' '.join(q['product'])
-            start = cds.location.start.real
-            end = cds.location.start.real
-            pos = f"[{r.id}:c({start}..{end})]"
-            _r.id = f"{annoate_name}_{gname}|{locus_tag} {pos} [{product}]"
-            new_proteins.append(_r)
-    with open(join(odir, f"{gname}.faa"), 'w') as f1:
-        SeqIO.write(new_proteins, f1, format='fasta-2line')
+    if not exists(odir):
+        os.makedirs(odir)
+    for g in tqdm(glob(join(indir, '*', '*.gbk'))):
+        records = SeqIO.parse(g, format='genbank')
+        gname = basename(dirname(g))
+        new_proteins = []
+        annoate_name = '_'.join(g2name.get(gname, []))
+        if not annoate_name:
+            continue
+        for r in records:
+            CDS_records = [_ for _ in r.features if _.type == 'CDS']
+            for cds in CDS_records:
+                _r = cds.translate(r)
+                _r.id = _r.name = _r.description = ''
+                q = cds.qualifiers
+                locus_tag = q['locus_tag'][0]
+                product = ' '.join(q['product'])
+                start = cds.location.start.real
+                end = cds.location.start.real
+                pos = f"[{r.id}:c({start}..{end})]"
+                _r.id = f"{annoate_name}_{gname}|{locus_tag} {pos} [{product}]"
+                new_proteins.append(_r)
+        with open(join(odir, f"{gname}.faa"), 'w') as f1:
+            SeqIO.write(new_proteins, f1, format='fasta-2line')
+
+if __name__ == '__main__':
+    # indir = './pipelines_o/prokka_o'
+    # mode = 'prokka'
+    # annotate_16s = './matchResults.csv'
+    # odir = './new_proteins'
+    import sys
+    assert len(sys.argv) == 4
+    # modify_prokka_o.py ./pipelines_o/prokka_o ./new_proteins ./matchResults.csv
+    def process_path(path):
+        if not '/' in path:
+            path = './' + path
+        return abspath(path)
+    indir = process_path(sys.argv[1])
+    odir = process_path(sys.argv[2])
+    annotate_16s = process_path(sys.argv[3])
+    main(indir,odir,annotate_16s)
